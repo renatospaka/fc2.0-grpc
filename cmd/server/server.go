69c195c1ea7c9a997756cb866652f71d2cd3e2e4
Renato Spakauskas <renatospaka@gmail.com>
2021-01-11 18:34:31 -0300

    feat: :construction: 1st gRPC client call

--- cmd/server/server.go
+++ cmd/server/server.go
@@ -4,16 +4,22 @@ import (
 	"log"
 	"net"
 
+	"github.com/renatospaka/fc2.0-grpc/pb"
+	"github.com/renatospaka/fc2.0-grpc/services"
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/reflection"
 )
 
 func main() {
 	listen, err := net.Listen("tcp", "localhost:50051")
 	if err != nil {
-		log.Fatalf("Could not connect: %w", err)
+		log.Fatalf("Could not connect: %v", err)
 	}
 
 	grpcServer := grpc.NewServer()
+	pb.RegisterUserServiceServer(grpcServer, services.NewUserService())
+	reflection.Register(grpcServer)
+	
 	if err = grpcServer.Serve(listen); err != nil {
 		log.Fatalf("Could not serve: %v", err)
 	}
--- go.mod
+++ go.mod
@@ -4,7 +4,8 @@ go 1.15
 
 require (
 	github.com/golang/protobuf v1.4.2
-	google.golang.org/grpc v1.34.0 // indirect
+	github.com/ktr0731/evans v0.9.1 // indirect
+	google.golang.org/grpc v1.34.0
 	google.golang.org/grpc/cmd/protoc-gen-go-grpc v1.0.1 // indirect
 	google.golang.org/protobuf v1.25.0 // indirect
 )
