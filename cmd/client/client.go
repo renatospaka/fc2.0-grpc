ae9f3e44dd4538dfca5fb41da7d0b6f1916a5152
Renato Spakauskas <renatospaka@gmail.com>
2021-01-11 18:55:51 -0300

    feat: :construction: 1st gRPC client ever

---
+++ cmd/client/client.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+
+	"github.com/renatospaka/fc2.0-grpc/pb"
+	"google.golang.org/grpc"
+)
+
+func main() {
+	connection, err := grpc.Dial("localhost:50051", grpc.WithInsecure())
+	if err != nil {
+		log.Fatalf("Could not connect to gRPC server: %v", err)
+	}
+	defer connection.Close()
+
+	client := pb.NewUserServiceClient(connection)
+	AddUser(client)
+}
+
+func AddUser(client pb.UserServiceClient) {
+	req := &pb.User{
+		Id:                   "0",
+		Name:                 "João Melão",
+		Email:                "melao@joao.com",
+	}
+
+	res, err := client.AddUser(context.Background(), req)
+	if err != nil {
+		log.Fatalf("Could not make gRPC request: %v", err)
+	}
+
+	fmt.Println(res)
+}
