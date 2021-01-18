2fe7af8d2fb78d3dfa17daa5e2f6cb68424b6dc3
Renato Spakauskas <renatospaka@gmail.com>
2021-01-18 14:10:51 -0300

    feat: :sparkles: adding server streaming

--- services/user.go
+++ services/user.go
@@ -3,14 +3,10 @@ package services
 import (
 	"context"
 	"fmt"
+	"time"
 
 	"github.com/renatospaka/fc2.0-grpc/pb"
 )
-
-// type UserServiceServer interface {
-// 	AddUser(context.Context, *User) (*User, error)
-// 	mustEmbedUnimplementedUserServiceServer()
-// }
 
 type UserService struct {
 	pb.UnimplementedUserServiceServer
@@ -20,11 +16,55 @@ func NewUserService() *UserService {
 	return &UserService{}
 }
 
+// type UserServiceServer interface {
+// 	AddUser(context.Context, *User) (*User, error)
+// 	mustEmbedUnimplementedUserServiceServer()
+// }
 func (*UserService) AddUser(ctx context.Context, req *pb.User) (*pb.User, error) {
 	fmt.Println(req.Name)
 	return &pb.User{
-		Id:                   "1234",
-		Name:                 req.GetName(),
-		Email:                req.GetEmail(),
+		Id:    "1234",
+		Name:  req.GetName(),
+		Email: req.GetEmail(),
 	}, nil
+}
+
+// type UserServiceClient interface {
+// 	AddUser(ctx context.Context, in *User, opts ...grpc.CallOption) (*User, error)
+// 	AddUserVerbose(ctx context.Context, in *User, opts ...grpc.CallOption) (UserService_AddUserVerboseClient, error)
+// }
+func (*UserService) AddUserVerbose(req *pb.User, res pb.UserService_AddUserVerboseServer) error {
+	res.Send(&pb.UserResultStreamUser{
+		Status: "Init",
+		User:   &pb.User{},
+	})
+	time.Sleep(time.Second * 3)
+
+	res.Send(&pb.UserResultStreamUser{
+		Status: "Creating",
+		User:   &pb.User{},
+	})
+	time.Sleep(time.Second * 3)
+
+	res.Send(&pb.UserResultStreamUser{
+		Status: "User has been created",
+		User: &pb.User{
+			Id:    "1234",
+			Name:  req.GetName(),
+			Email: req.GetEmail(),
+		},
+	})
+	time.Sleep(time.Second * 3)
+
+	res.Send(&pb.UserResultStreamUser{
+		Status: "Completed",
+		User: &pb.User{
+			Id:    "1234",
+			Name:  req.GetName(),
+			Email: req.GetEmail(),
+		},
+	})
+	time.Sleep(time.Second * 3)
+
+	return nil
 }
